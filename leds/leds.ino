f6b8a174b1c17fd10f58e2c8ea1042459123b403
Kevin Torregrosa <142925854+MrManini@users.noreply.github.com>
2024-05-01 18:50:58 -0500

    Update leds.ino

--- leds/leds.ino
+++ leds/leds.ino
@@ -1,51 +1,88 @@
-/* Prueba de tiras LED inteligentes WS2812B / WS2812
-by: www.elprofegarcia.com
-Debe instalar la libreria "Adafruit_NeoPixel.h" para que funcione el programa
-Conexiones:
-ARDUINO   TIRA LED
-  5V        +5V ROJO
-  GND       GND BLANCO
-  3         Din Verde
-*/
-#include <Adafruit_NeoPixel.h>  // importa libreria, debe indtalarla previamente
+#include <SPI.h>
+#include <WiFi.h>
+#include <WiFiNINA.h>
+#include <Adafruit_NeoPixel.h>
 
-const int nleds = 30;   // Numero de LEDs a Probar
-int retardo = 5;  // más alto mas lenta la secuencia
-int brillo = 30;  // Brillo del LED
-Adafruit_NeoPixel tira = Adafruit_NeoPixel(nleds, 3, NEO_GRB + NEO_KHZ800);  // Pin 3 del Arduino es salida de datos
+const int nleds = 30;
+int brightness = 30;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(nleds, 3, NEO_GRB + NEO_KHZ800);  // Arduino Pin 3 Data Output
 int nextLed = -1;
 int prevLed;
 bool leds[nleds];
 
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+char ssid[] = "";        // your network SSID (name)
+char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                 // your network key index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+WiFiServer server(80);
+
 void setup() {
-  Serial.begin(9600);
-  tira.begin();  // inicializacion de la tira
-  tira.setBrightness(brillo);  // Brillo de los LEDs
-  tira.show();
+  strip.begin();
+  strip.setBrightness(brightness);
+  strip.show();
+  
+  pinMode(LEDR, OUTPUT);
+  pinMode(LEDG, OUTPUT);
+  pinMode(LEDB, OUTPUT);
+
+  digitalWrite(LEDR, LOW);
+  digitalWrite(LEDG, LOW);
+  digitalWrite(LEDB, HIGH);
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    digitalWrite(LEDR, HIGH);
+    while (true);
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    status = WiFi.begin(ssid, pass);
+    delay(10000);
+  }
+  server.begin();                           // start the web server on port 80
+  digitalWrite(LEDG, HIGH);
+  digitalWrite(LEDB, LOW);
 }
 
+
 void loop() {
-  if (Serial.available() > 0){
-    nextLed = Serial.parseInt();
+  WiFiClient client = server.available();   // listen for incoming clients
+  digitalWrite(LEDB, LOW);
+  if (client) {                             // if you get a client,
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        digitalWrite(LEDB, HIGH);
+        if (c != '\r'){
+          currentLine += c;
+        }
+      }
+    }
+    // close the connection:
+    client.stop();
+    nextLed = currentLine.toInt();
     if (0 <= nextLed && nextLed < nleds){
       leds[nextLed] = !leds[nextLed];
-    } else if (nextLed == 1000){
+    } else if (nextLed == -1){
       for (int i = 0; i < nleds; i++){
         leds[i] = false;
       }
-    } else if (nextLed == 999){
+    } else if (nextLed == 99){
       for (int i = 0; i < nleds; i++){
         leds[i] = true;
       }
     }
-  }
-
-  for (int i = 0; i < nleds; i++) {
-    if (leds[i]){
-      tira.setPixelColor(i, 255, 255, 255);  // Blanco
-    } else {
-      tira.setPixelColor(i, 0, 0, 0);  // Apagado
+    for (int i = 0; i < nleds; i++) {
+      if (leds[i]){
+        strip.setPixelColor(i, 255, 255, 255);  // White
+      } else {
+        strip.setPixelColor(i, 0, 0, 0);  // Off
+      }
+      strip.show();
     }
-    tira.show();
   }
 }
