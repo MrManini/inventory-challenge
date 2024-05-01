9bc2a1edc04cb8bfd22d9de5c889f9af6e7a2f32
kevin <kevintorregrosa2304@gmail.com>
2024-05-01 18:47:10 -0500

    finalised pick2light code thru wifi

---
+++ leds/pick2light.py
@@ -0,0 +1,18 @@
+import socket
+
+HOST = 'Arduino IP'  # Puedes encontrar la IP del Arduino en el monitor serial del Arduino IDE
+PORT = 80
+
+def send_command(command):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(command.encode())
+
+if __name__ == "__main__":
+    while True:
+        cmd = input("Enviar datos a Arduino Nano RP2040 Connect: ")
+        try:
+            send_command(cmd)
+        except Exception as e:
+            print(f"Error: {e}")
+            
