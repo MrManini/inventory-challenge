c46141481026e54cb75daf0303b4cd99fc7c74eb
jhonatan <jhonatan-204@hotmail.com>
2024-06-01 14:55:21 -0500

    Cambios finales

--- Pagina_web/gestor.py
+++ Pagina_web/gestor.py
@@ -5,14 +5,11 @@ from threading import Timer
 
 app = Flask(__name__, static_folder="static")
 
-#app.config['MYSQL_HOST'] = 'localhost'
-#app.config['MYSQL_USER'] = 'central'
-#app.config['MYSQL_PASSWORD'] = 'Central-Stati0n'
-#app.config['MYSQL_DB'] = 'storage'
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'jhonatan'
-app.config['MYSQL_DB'] = 'datostiempo'
+app.config['MYSQL_USER'] = 'central'
+app.config['MYSQL_PASSWORD'] = 'Central-Stati0n'
+app.config['MYSQL_DB'] = 'storage'
+
 
 mysql = pymysql.connect(
     host=app.config['MYSQL_HOST'],
