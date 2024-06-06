a12c4cb65a574da756400b0f6a7fc11fb3a774ef
jhonatan <jhonatan-204@hotmail.com>
2024-06-06 00:49:23 -0500

    a

--- Pagina_estaciones/conector.py
+++ Pagina_estaciones/conector.py
@@ -5,17 +5,17 @@ from datetime import datetime, timedelta
 
 app = Flask(__name__, static_folder="static")
 
-f = open('Pagina_estaciones/estacion.json')
-data = json.load(f)
-f.close()
-
+#f = open('Pagina_estaciones/estacion.json')
+#data = json.load(f)
+#f.close()
+'''
 mysql = pymysql.connect(
     host = data['host'],
     user = data['user'],
     password = data['password'],
     db = data['database'],
     cursorclass = pymysql.cursors.DictCursor
-)
+)'''
 
 @app.route('/', methods=["POST", "GET"])
 def index():
@@ -28,7 +28,11 @@ def index():
         minutos_inactivo = int(request.form["minutos_inactivo"])
         segundos_inactivo = int(request.form["segundos_inactivo"])
         milisegundos_inactivo = int(request.form["milisegundos_inactivo"])*10
-        
+        tipo_produccion = request.form.get("tipo_de_produccion", "off")  # Usar get() con un valor predeterminado
+        if tipo_produccion=="on":
+            print("Hola")
+        else:
+            print("mundo")
         work = timedelta(hours=horas, minutes=minutos, seconds=segundos, milliseconds=milisegundos)
         timeout = timedelta(hours=horas_inactivo, minutes=minutos_inactivo, seconds=segundos_inactivo, milliseconds=milisegundos_inactivo)
         total = work + timeout
@@ -36,6 +40,8 @@ def index():
 
     return render_template("index.html")
 
+
+'''
 @app.route("/handle_option", methods=["POST"])
 def handle_option():
     selected_option = request.json["option"]
@@ -44,18 +50,18 @@ def handle_option():
     else:
         print("Mundo")
     return jsonify({"message": ""}) # No borrar
-
+'''
 
 
 def independent_stations(work, timeout, total):
     now = datetime.now(pytz.timezone('America/Bogota'))
     now = now.strftime('%Y-%m-%d %H:%M:%S')
-    station = data['user'][-1]
+    #station = data['user'][-1]
 
-    cursor = mysql.cursor()
+    #cursor = mysql.cursor()
     query = "INSERT INTO `stations` VALUES (%s, %s, %s, %s, %s)"
-    cursor.execute(query, (now, station, work, timeout, total))
-    mysql.commit()
+    #cursor.execute(query, (now, station, work, timeout, total))
+    #mysql.commit()
 
 def open_browser():
     url = "http://localhost:5000"
